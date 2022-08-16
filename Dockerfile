54a57a7663475e192449f2fcc0b076a7da4fa34e
Rami Chasygov <4y9hx1bs1@mozmail.com>
2022-08-16 07:46:15 +0300

    feat: restore dev environment

--- Dockerfile
+++ Dockerfile
@@ -5,9 +5,10 @@ WORKDIR /usr/src/app
 COPY package.json .
 
 RUN npm install
+RUN npm run build
 
 COPY . .
 
-CMD ["npm", "run", "start"]
+CMD ["npm", "run", "start:prod"]
 
 EXPOSE 3000
--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -14,7 +14,7 @@ export const databaseProviders = [
     async useFactory() {
       const dataSource = new DataSource({
         type: 'postgres',
-        host: 'pg',
+        host: 'localhost',
         port: 5432,
         username: 'postgres',
         password: 'example',
