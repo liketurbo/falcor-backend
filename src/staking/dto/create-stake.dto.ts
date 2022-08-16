66a8ed1bdf12b30e82382c0affcc7484d6803f3f
Rami Chasygov <4y9hx1bs1@mozmail.com>
2022-08-16 20:35:35 +0300

    feat: add class-validator

--- src/staking/dto/create-stake.dto.ts
+++ src/staking/dto/create-stake.dto.ts
@@ -1,9 +1,18 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { ArrayContains, IsPositive } from 'class-validator';
+
+import {
+  INTERVAL_3_MONTHS,
+  INTERVAL_6_MONTHS,
+  INTERVAL_9_MONTHS,
+} from '../constants/intervals.constants';
 
 export class CreateStakeDto {
+  @ArrayContains([INTERVAL_3_MONTHS, INTERVAL_6_MONTHS, INTERVAL_9_MONTHS])
   @ApiProperty()
   period: string;
 
+  @IsPositive()
   @ApiProperty()
   amount: number;
 }
